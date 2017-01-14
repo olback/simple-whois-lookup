22810d3ae34c0ff37d9d4a9fcff1e196bc88df75
olback <github@olback.net>
2017-01-14 21:34:29 +0100

    Removed unnecessary files.

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $ip = htmlspecialchars($_GET["ip"]);
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="fa/css/font-awesome.css" rel="stylesheet">
+    <link href="fa/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
