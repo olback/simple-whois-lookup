e7ea64bd37c8f2b35deeb5f7dbd634aa0c13fca8
olback <git@olback.net>
2017-10-05 19:22:52 +0200

    Complete remake of the backend

--- res/settings.php
+++ res/settings.php
@@ -1,29 +1,8 @@
 <?php
-// Set hex for mobile top-bar-color
-// Default nav-bar color: #292b2c
-$col = "#292b2c";
+// Title
+$title = 'WHOIS Lookup';
 
-// Timeout in seconds for getting server software info.
-// Recomended: 5
-$time = "5";
+// Max lookup time in seconds. Default: 5
+$timeout = 10;
 
-// Set "ad"-html, for an example: Made by olback.
-$ad = 'Made by <i class="fa fa-github cCol"></i> <a href="https://twitter.com/mrolback" target="_blank">olback</a>';
-
-// This is the css for your "ad".
-$adCss = "
-<style>
-.customad {
-  position: fixed;
-  bottom: 8px;
-  right: 20px;
-  z-index: 9001; /* It's over 9000! */
-}
-
-.cCol {
-  /* color: #55acee; */ /* Twitter blue */
-  color: #000;
-}
-</style>
-";
 ?>
